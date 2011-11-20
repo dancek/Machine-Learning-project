b5b888f1c5da2690fbb38f0a39aa16ccb63856ef
Eric <ekku@bat.(none)>
2011-11-20 20:58:53 +0200

    koodit

--- bmix_pred.m
+++ bmix_pred.m
@@ -14,14 +14,14 @@ system(['./bmix-1.11/bin/bmix_train --data data/train_ham' id '.data --clusters 
     num2str(c_ham) ' -t 100 -r 0.000001 -q --model-out models/train_ham' id '.model']);
 system(['./bmix-1.11/bin/bmix_train --data data/train_spam' id '.data --clusters ' ...
     num2str(c_spam) ' -t 100 -r 0.000001 -q --model-out models/train_spam' id '.model']);
-system(['./bmix-1.11/bin/bmix_like --data data/validation' id '.data --model-in models/train_ham' id '.model -s > data/ham_likelihoods' id '.txt']);
-system(['./bmix-1.11/bin/bmix_like --data data/validation' id '.data --model-in models/train_spam' id '.model -s > data/spam_likelihoods' id '.txt']);
+system(['./bmix-1.11/bin/bmix_like --data data/validation' id ...
+    '.data --model-in models/train_ham' id '.model -s > data/ham_likelihoods' id '.txt']);
+system(['./bmix-1.11/bin/bmix_like --data data/validation' id ...
+    '.data --model-in models/train_spam' id '.model -s > data/spam_likelihoods' id '.txt']);
 
 ham_ls = load(['data/ham_likelihoods' id '.txt']);
 spam_ls = load(['data/spam_likelihoods' id '.txt']);
 
-%size(spam_ls)
-%size(ham_ls)
 likelihoods = exp(spam_ls)./(exp(ham_ls) + exp(spam_ls));
 
 system(['rm data/train_ham' id '.data']);
